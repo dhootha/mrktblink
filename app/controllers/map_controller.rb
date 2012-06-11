78941062edc6ffed76e09e6969d28f088d83f3cf
Test Machine <testmachine@ubuntu.ubuntu-domain>
2012-06-11 08:06:06 +0200

    fetching data from api

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -4,4 +4,12 @@ class MapController < ApplicationController
     render :layout => "map"
   end
 
+  def find_data
+    market = 'Dow'
+    #debugger
+    begin
+      FetchData.fetch_data_from_api(market)
+    rescue
+    end
+  end
 end
---
+++ lib/fetch_data.rb
@@ -0,0 +1,18 @@
+require 'net/http'
+require 'net/https'
+require 'uri'
+require 'json'
+module FetchData
+  
+  def self.fetch_data_from_api(market)
+  	#query = query.gsub("\;","\\\;")
+  	query = APP_CONFIG[market]['url']
+  	begin
+	  request = URI.parse(query)
+	  request = Net::HTTP.get_response(request).body
+	rescue
+	  return false
+	end
+    response = JSON.parse(request)
+  end    
+end
