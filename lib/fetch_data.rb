e41334de586f54c31fa8204b6465cd9994a30013
Test Machine <testmachine@ubuntu.ubuntu-domain>
2012-06-15 17:18:20 +0200

    redis and resque tasks

--- lib/fetch_data.rb
+++ lib/fetch_data.rb
@@ -5,13 +5,14 @@ require 'json'
 module FetchData
   
   def self.fetch_data_from_api(market)
-  	query = APP_CONFIG[market]['url']
+  	query = APP_CONFIG_MARKETS[market]['url']
   	begin
-	  request = URI.parse(query)
-	  request = Net::HTTP.get_response(request).body
-	rescue
-	  return false
-	end
-    response = JSON.parse(request)
+	    request = URI.parse(query)
+	    request = Net::HTTP.get_response(request).body
+	  rescue
+	    return false
+	  end
+    response = request.to_json
+    return response    
   end
 end
