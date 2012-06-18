2ce2b63cf0074b1f8b5ba29fac3fd05400037f2f
Test Machine <testmachine@ubuntu.ubuntu-domain>
2012-06-18 14:53:56 +0200

    redis and resque changes

--- app/workers/get_market_data.rb
+++ app/workers/get_market_data.rb
@@ -2,6 +2,7 @@ class GetMarketData
   @queue = :market
   def self.perform(market)
     response = FetchData.fetch_data_from_api(market[0])
-    $redis.set("data_#{market[0]}",response)    
+    $redis.set("data_#{market[0]}",response)
+    puts "hi resque"
   end
 end
