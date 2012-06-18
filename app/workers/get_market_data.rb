b28b01eff7c677e469c50c24b7133dc72a42b43a
Test Machine <testmachine@ubuntu.ubuntu-domain>
2012-06-18 12:03:13 +0200

    redis and resque changes

--- app/workers/get_market_data.rb
+++ app/workers/get_market_data.rb
@@ -1,8 +1,7 @@
 class GetMarketData
   @queue = :market
-  def self.perform(market)    
-    puts "helloooo Resque...."    
-    response = FetchData.fetch_data_from_api(market[0])    
+  def self.perform(market)
+    response = FetchData.fetch_data_from_api(market[0])
     $redis.set("data_#{market[0]}",response)    
   end
 end
--- lib/tasks/store_data_to_redis.rake
+++ lib/tasks/store_data_to_redis.rake
@@ -1,6 +1,5 @@
 task :store_data_to_redis => :environment do
   APP_CONFIG_MARKETS.each do |market|
     Resque.enqueue(GetMarketData,market)    
-  end
-  puts "done"
+  end  
 end
