aee27ce9b5a2da1d878eee0d7efbc5f7fb62de08
Test Machine <testmachine@ubuntu.ubuntu-domain>
2012-07-19 14:58:50 +0200

    resque

--- app/workers/get_market_data.rb
+++ app/workers/get_market_data.rb
@@ -1,15 +1,13 @@
 class GetMarketData
   @queue = :market
   def self.perform(market)
-    response = FetchData.fetch_data_from_api(market[0])
+    response = FetchData.fetch_data_from_api(market[0])    
     $redis.set("data_#{market[0]}",response)
     if MarketData.exist?(:market_name => market[0])      
       market = MarketData.find_by_market_name(market[0])
       if market.update_attributes(:market_data => response)
-        puts market[0]
-      else
-        puts "not updated"
-      end
+        puts market.market_name
+      end  
     else
       puts "data does not exist"
       MarketData.create(:market_name => market[0],:country => APP_CONFIG_MARKETS[market[0]]["country"],:market_data => response)
