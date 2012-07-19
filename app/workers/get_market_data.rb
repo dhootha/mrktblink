b5ff88ec45e80500c2179bc9bf1fdf38e1c91645
Test Machine <testmachine@ubuntu.ubuntu-domain>
2012-07-19 14:59:59 +0200

    resque

--- app/workers/get_market_data.rb
+++ app/workers/get_market_data.rb
@@ -12,6 +12,6 @@ class GetMarketData
       puts "data does not exist"
       MarketData.create(:market_name => market[0],:country => APP_CONFIG_MARKETS[market[0]]["country"],:market_data => response)
     end
-    puts "hi resque"
+    #puts "hi resque"
   end
 end
