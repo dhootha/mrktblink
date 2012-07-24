58158bc6e1ff6d35fc30e8550b3f8fe4c23abb59
Reki Hattori <reki@agencyprotocol.com>
2012-07-24 18:08:12 -0400

    removing usa string

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -46,13 +46,13 @@ class MapController < ApplicationController
     else
       market_data_value = 'does not exist'
     end  
-    close = "12341234.1234".to_f 
-    prev = "12454354.5334".to_f
-    percent = (((close - prev) / (prev)) * 100).round(3)
-    change = percent > 0 ? 'up' : 'down'
-    sign = percent > 0 ? '+' : ''
-    points = (close - prev).round(3)
-    market_data_value = [{:market_name=>"DOW", :previous_close=>prev, :close=>close, :change=>change, :percent=>percent, :country=>"USA", :sign=>sign, :points=>points}, {:market_name=>"NASDAQ", :previous_close=>prev, :close=>close, :change=>change, :percent=>percent, :country=>"USA", :sign=>sign, :points=>points}, {:market_name=>"S&P", :previous_close=>prev, :close=>close, :change=>change, :percent=>percent, :country=>"USA", :sign=>sign, :points=>points}]
+    # close = "12341234.1234".to_f 
+    # prev = "12454354.5334".to_f
+    # percent = (((close - prev) / (prev)) * 100).round(3)
+    # change = percent > 0 ? 'up' : 'down'
+    # sign = percent > 0 ? '+' : ''
+    # points = (close - prev).round(3)
+    # market_data_value = [{:market_name=>"DOW", :previous_close=>prev, :close=>close, :change=>change, :percent=>percent, :country=>"USA", :sign=>sign, :points=>points}, {:market_name=>"NASDAQ", :previous_close=>prev, :close=>close, :change=>change, :percent=>percent, :country=>"USA", :sign=>sign, :points=>points}, {:market_name=>"S&P", :previous_close=>prev, :close=>close, :change=>change, :percent=>percent, :country=>"USA", :sign=>sign, :points=>points}]
     json = {:success => true, :market_data => market_data_value, :content_to_replace => render_to_string(:partial => 'news_feeds_content', :locals => {:country => params[:country_name]})}
     render :json => json
   end
