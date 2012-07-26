c3748013b1cdcece89d17ae6f4cb9e304a44a79f
Reki Hattori <reki@agencyprotocol.com>
2012-07-26 04:02:54 -0400

    test each country

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -65,7 +65,7 @@ class MapController < ApplicationController
     # APP_CONFIG_MARKETS.each do |market|
     #   countries << market[1]["country"]
     # end
-    countries = ["Alaska", "Argentina", "Brazil", "Mexico", "Chile"]
+    countries = ["Alaska", "Argentina", "Brazil", "Mexico", "Chile", "Peru", "Colombia", "Canada", "Austria", "Belgium", "France", "Germany"]
     countries.uniq.each do |country|
       markets = MarketData.where(country: country)
       begin
